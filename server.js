c835913d69eb265583251d36a747ce693151d7c4
Steven Rafferty <stevenrafferty2@protonmail.com>
2021-02-18 18:41:16 +0000

    fixed typo in url

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/api/v1/notification/meeting', (req, res) => {
+app.post('/api/v1/notifications/meeting', (req, res) => {
   const { method, path, status } = req.body;
   console.log('body', req.body);
   console.log('headers', req.headers);
