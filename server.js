ad2d0e90502d5eaf7ea7a945584af3a198a9eae5
Steven Rafferty <stevenrafferty2@protonmail.com>
2021-02-18 18:38:46 +0000

    added console.log

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/api/v1/notification/meeting', (req, res) => {
-  console.log(req.body);
+  const { method, path, status } = req.body;
+  console.log('body', req.body);
+  console.log('headers', req.headers);
+  console.log(`
+    Method: ${method}
+    Path: ${path}
+    Method: ${status}
+    Authorization: ${req.headers.authorization}
+  `);
 });
 
 app.listen(port, () => console.log(`Listening on PORT: ${port}`));
