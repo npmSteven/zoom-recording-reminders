7be1a2d79c879024a42c1cd47d41d3f041775713
Steven Rafferty <stevenrafferty2@protonmail.com>
2021-02-18 18:50:07 +0000

    added log

--- server.js
+++ server.js
@@ -8,16 +8,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/api/v1/notifications/meeting', (req, res) => {
-  if (req.body.event) return res.sendStatus(401);
-  const { event, payload } = req.body;
-  console.log('event', event);
-  console.log('payload', payload);
-  switch (event) {
-    case 'meeting.created': {
+  console.log(req.body);
+  // if (req.body.event) return res.sendStatus(401);
+  // const { event, payload } = req.body;
+  // console.log('event', event);
+  // console.log('payload', payload);
+  // switch (event) {
+  //   case 'meeting.created': {
 
-    }
-  }
-  res.sendStatus(200);
+  //   }
+  // }
+  // res.sendStatus(200);
 });
 
 app.listen(port, () => console.log(`Listening on PORT: ${port}`));
